6679d14f414d710ae9b83d0815b3194d589200c7
raysulkobir <raysulkobir97@gmail.com>
2022-08-07 22:15:13 +0600

    first commit

---
+++ 17.1 - Discussion of PHP settings and reporting.php
@@ -0,0 +1,7 @@
+<?php
+    // error_reporting(E_WARNING || E_NOTICE);
+    ini_set('display_errors', 0);
+    echo 12/0;
+    echo $book;
+
+    // echo get_cfg_var('cfg_file_path');
---
+++ 17.2 - PHP Error Log - Logging errors to a file.php
@@ -0,0 +1,11 @@
+<?php
+
+//! change error reporting to E_WARNING || E_NOTICE
+error_reporting(E_WARNING || E_NOTICE);
+//! php.ini file: display_errors = 0
+ini_set('display_errors', 0);
+//! php.ini file: error_log = /path/to/file.log
+ini_set('error_log', '/path/to/file.log');
+//! php.ini file: log_errors = On
+ini_set('log_errors', 'On');
+ 
